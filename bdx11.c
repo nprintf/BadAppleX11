72de24e6f9d7f39b530d05b88f7e2301825351eb
ecksemzee <45754441+ecksemzee@users.noreply.github.com>
2021-10-21 17:35:05 +0000

    Add files via upload

--- bdx11.c
+++ bdx11.c
@@ -1,6 +1,6 @@
 #define PIXELWIDTH 4
 #define PIXELHEIGHT 8
-#define DELAYINMICROSECONDS 33125
+#define DELAYINMICROSECONDS 33125 /* mess around with this setting if song is off with the "video" */
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -13,85 +13,74 @@
 Display* WindowDisplay;
 Window WindowObj;
 int WindowScreen;
-
 char Frame[23][64];
 
 void CleanUp(void) {
-    XDestroyWindow(WindowDisplay, WindowObj);
-    XCloseDisplay(WindowDisplay);
+	XDestroyWindow(WindowDisplay, WindowObj);
+	XCloseDisplay(WindowDisplay);
 }
 
 void Die(const char* Format, ...) {
-    if (WindowObj) {
-        CleanUp();
-    }
+	if (WindowObj) CleanUp();
 	va_list VArgs;
 	va_start(VArgs, Format);
 	vfprintf(stderr, Format, VArgs);
 	va_end(VArgs);
-    exit(1);
+   	exit(1);
 }
 
 int ReadFrame(unsigned int Iteration) {
-    int Ret = 0;
-    char FName[17];
-    snprintf(FName, 17, "frames/%u", Iteration);
-    FILE* File = fopen(FName, "r");
-    if (!File) {
-        Ret = 1;
-    } else {
-        int i = 0;
-        char Buffer[66];
-        while (fgets(Buffer, 66, File) != NULL) {
-            strncpy(Frame[i], Buffer, 64);
-            i++;
-        }
-        fclose(File);
-    }
-    return Ret;
+	int Ret = 0;
+	char FName[17];
+	snprintf(FName, 17, "frames/%u", Iteration);
+	FILE* File = fopen(FName, "r");
+	if (!File) Ret = 1;
+	else {
+		int i = 0;
+		char Buffer[66];
+		while (fgets(Buffer, 66, File) != NULL) {
+			strncpy(Frame[i], Buffer, 64);
+			i++;
+		}
+        	fclose(File);
+	}
+	return Ret;
 }
 
 void Init(void) {
-    if ((WindowDisplay = XOpenDisplay(NULL)) == NULL)
-        Die("Cannot open display\n");
-    WindowScreen = DefaultScreen(WindowDisplay);
-    WindowObj = XCreateSimpleWindow(WindowDisplay, RootWindow(WindowDisplay, WindowScreen), 10, 10, PIXELWIDTH * 63, PIXELHEIGHT * 23,  1, BlackPixel(WindowDisplay, WindowScreen), WhitePixel(WindowDisplay, WindowScreen));
-    XStoreName(WindowDisplay, WindowObj, "Bad Apple!!");
-    XMapWindow(WindowDisplay, WindowObj);
+	if ((WindowDisplay = XOpenDisplay(NULL)) == NULL) Die("Cannot open display\n");
+	WindowScreen = DefaultScreen(WindowDisplay);
+	WindowObj = XCreateSimpleWindow(WindowDisplay, RootWindow(WindowDisplay, WindowScreen), 10, 10, PIXELWIDTH * 63, PIXELHEIGHT * 23,  1, BlackPixel(WindowDisplay, WindowScreen), WhitePixel(WindowDisplay, WindowScreen));
+	XStoreName(WindowDisplay, WindowObj, "Bad Apple!!");
+	XMapWindow(WindowDisplay, WindowObj);
 }
 
-void MainLoop() {
-    int Open = 1;
-    XSelectInput(WindowDisplay, WindowObj, ExposureMask | KeyPressMask);
-    while (Open) {
-        XEvent Event;
-        XNextEvent(WindowDisplay, &Event);
-        if (Event.type == Expose) {
-            for (unsigned i = 1; i < 6574; i++) {
-                if (ReadFrame(i) != 0)
-                    Die("Missing frame '%u'\n", i);
-                int PosX, PosY = 0;
-                for (int j = 0; j < 24; j++) {
-                    PosX = 0;
-                    for (int k = 0; k < 64; k++) {
-                        if (Frame[j][k] == '1')
-                            XFillRectangle(WindowDisplay, WindowObj, DefaultGC(WindowDisplay, WindowScreen), PosX, PosY, PIXELWIDTH, PIXELHEIGHT);
-                        PosX += PIXELWIDTH;
-                    }
-                    PosY += PIXELHEIGHT;
-                    XFlush(WindowDisplay);
-                }
-                usleep(DELAYINMICROSECONDS);
-                XClearWindow(WindowDisplay, WindowObj);
-            }
-            Open = 0;
-        }
-    }
+void MainLoop() {	
+	XSelectInput(WindowDisplay, WindowObj, ExposureMask | KeyPressMask);
+	XEvent Event;
+	XNextEvent(WindowDisplay, &Event);
+	if (Event.type == Expose) {
+		for (unsigned i = 1; i < 6574; i++) {
+			if (ReadFrame(i) != 0) Die("Missing frame '%u'\n", i);
+			int PosX, PosY = 0;
+			for (int j = 0; j < 24; j++) {
+				PosX = 0;
+				for (int k = 0; k < 64; k++) {
+					if (Frame[j][k] == '1') XFillRectangle(WindowDisplay, WindowObj, DefaultGC(WindowDisplay, WindowScreen), PosX, PosY, PIXELWIDTH, PIXELHEIGHT);
+					PosX += PIXELWIDTH;
+				}
+				PosY += PIXELHEIGHT;
+				XFlush(WindowDisplay);
+			}
+			usleep(DELAYINMICROSECONDS);
+			XClearWindow(WindowDisplay, WindowObj);
+		}
+	}
 }
 
 int main(void) {
-    Init();
-    MainLoop();
-    CleanUp();
-    return 0;
+	Init();
+	MainLoop();
+	CleanUp();
+	return 0;
 }
